3200cd32492d1b68b83e8998735aa9f46fa92013
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-07 07:33:08 +0530

    Create note1.js

---
+++ note1.js
@@ -0,0 +1,38 @@
+let b, i, u;
+function Aa(b) {
+    b=document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.fontWeight = "bold";
+}
+function Aa1(i) {
+     i=document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.fontStyle = "italic";
+}
+function Aa2(u) {
+   
+    
+     u=document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.textDecoration = "underline";}
+     
+
+     
+
+let bl, p, y, w, pr, v, g, r;
+function blue(bl) {
+      bl= document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color = "blue";}
+
+function pink(p) {
+    document.getElementById("typeSomething").value = document.getElementById("pink").style.color = "pink";
+}
+function  yellow(y){
+    document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color="yellow";
+}
+function white(w){
+    document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color="white";
+}
+function purple(pr){
+    document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color="purple";
+
+}
+function violet(v){
+    document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color="violet";
+}
+function green(g){
+    document.getElementById("typeSomething").value = document.getElementById("typeSomething").style.color="green";
+}
